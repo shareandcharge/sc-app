a6483342ba7f8eea2f3872c12f269f1cc1c4d3fb
Joachim Klinkhammer <j@davedata.de>
2016-12-12 12:21:16 +0100

    Add location model and service (wired to mockup api)

---
+++ src/models/location.ts
@@ -0,0 +1,43 @@
+import {Serializable} from './serializable';
+
+export class Location implements Serializable<Location> {
+    id: any;
+    owner: string;
+    name: string;
+    description: string;
+    images: Array<string>;
+    contact: string;
+    latitude: string;
+    longitude: string;
+    address: string;
+    stations: any;
+
+    constructor() {
+        this.id = '';
+        this.owner = '';
+        this.name = '';
+        this.description = '';
+        this.images = [];
+        this.contact = '';
+        this.latitude = '';
+        this.longitude = '';
+        this.address = '';
+        this.stations = '';
+    }
+
+    deserialize(input) {
+
+        this.id = input.id;
+        this.owner = input.owner;
+        this.name = input.name;
+        this.description = input.description;
+        this.images = input.images;
+        this.contact = input.contact;
+        this.latitude = input.latitude;
+        this.longitude = input.longitude;
+        this.address = input.address;
+        this.stations = input.stations;
+
+        return this;
+    }
+}
--- src/services/location.service.ts
+++ src/services/location.service.ts
@@ -1,101 +1,84 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-// import { Observable } from 'rxjs/Observable';
-// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import {Http, Headers} from '@angular/http';
+import {Observable} from "rxjs";
+
 import 'rxjs/add/operator/map';
-
-// export interface MyLocation {
-//     id: number | string;
-//     createdAt: number;
-//     value: string;
-// }
-
-class Location {
-    id: string;
-    name: string;
-    latitude: string;
-    longitude: string;
-
-    constructor(location: any) {
-        if (location) {
-            this.id = location.id;
-            this.name = location.name;
-            this.latitude = location.latitude;
-            this.longitude = location.longitude;
-        }
-    }
-}
+import {Location} from "../models/location";
 
 @Injectable()
 export class LocationService {
-    // locations: Observable<MyLocation[]>;
-    // private _locations: BehaviorSubject<MyLocation[]>;
-    private baseUrl: string;
-    // private dataStore: {
-    //     locations: MyLocation[]
-    // };
+
+    private baseUrl: string = 'http://5834821b62f23712003730c0.mockapi.io/api/v1';
+    private contentHeader: Headers = new Headers({"Content-Type": "application/json"});
 
     constructor(private http: Http) {
-        this.baseUrl = 'http://5834821b62f23712003730c0.mockapi.io/api/v1';
-        // this.dataStore = { locations: [] };
-        // this._locations = <BehaviorSubject<MyLocation[]>>new BehaviorSubject([]);
-        // this.locations = this._locations.asObservable();
     }
 
-    getAll() {
-        return this.http.get(`${this.baseUrl}/locations`)
-            .map(response => response.json());
-        // this.http.get(`${this.baseUrl}/locations`).map(response => response.json()).subscribe(data => {
-        //     this.dataStore.locations = data;
-        //     this._locations.next(Object.assign({}, this.dataStore).locations);
-        // }, error => console.log('Could not load locations.'));
+    getLocations() {
+        return this.http.get(this.baseUrl + '/locations')
+            .map(res => {
+                let locations = [];
+                res.json().forEach(input => {
+                    locations.push(new Location().deserialize(input));
+                });
+                return locations;
+            })
+            .catch(this.handleError);
     }
 
-    // load(id: number | string) {
-    //     this.http.get(`${this.baseUrl}/locations/${id}`).map(response => response.json()).subscribe(data => {
-    //         let notFound = true;
-    //
-    //         this.dataStore.locations.forEach((item, index) => {
-    //             if (item.id === data.id) {
-    //                 this.dataStore.locations[index] = data;
-    //                 notFound = false;
-    //             }
-    //         });
-    //
-    //         if (notFound) {
-    //             this.dataStore.locations.push(data);
-    //         }
-    //
-    //         this._locations.next(Object.assign({}, this.dataStore).locations);
-    //     }, error => console.log('Could not load todo.'));
-    // }
-    //
-    // create(todo: Location) {
-    //     this.http.post(`${this.baseUrl}/locations`, JSON.stringify(todo))
-    //         .map(response => response.json()).subscribe(data => {
-    //         this.dataStore.locations.push(data);
-    //         this._locations.next(Object.assign({}, this.dataStore).locations);
-    //     }, error => console.log('Could not create todo.'));
-    // }
-    //
-    // update(todo: Location) {
-    //     this.http.put(`${this.baseUrl}/locations/${todo.id}`, JSON.stringify(todo))
-    //         .map(response => response.json()).subscribe(data => {
-    //         this.dataStore.locations.forEach((t, i) => {
-    //             if (t.id === data.id) { this.dataStore.locations[i] = data; }
-    //         });
-    //
-    //         this._locations.next(Object.assign({}, this.dataStore).locations);
-    //     }, error => console.log('Could not update todo.'));
-    // }
-    //
-    // remove(todoId: number) {
-    //     this.http.delete(`${this.baseUrl}/locations/${todoId}`).subscribe(response => {
-    //         this.dataStore.locations.forEach((t, i) => {
-    //             if (t.id === todoId) { this.dataStore.locations.splice(i, 1); }
-    //         });
-    //
-    //         this._locations.next(Object.assign({}, this.dataStore).locations);
-    //     }, error => console.log('Could not delete todo.'));
-    // }
+    searchLocations(bounds?: any) {
+        let checkBounds = <boolean>bounds;
+        return this.getLocations().map(res => {
+
+            let locations = [];
+            res.forEach(location => {
+                let lat = location.latitude;
+                let lng = location.longitude;
+
+                if (checkBounds && lat >= bounds.latFrom && lat <= bounds.latTo && lng >= bounds.lngFrom && lng <= bounds.lngTo) {
+                    locations.push(location);
+                }
+                else if (!checkBounds) {
+                    locations.push(location);
+                }
+
+            });
+            return locations;
+
+        });
+    }
+
+    getLocation(id): Observable<Location> {
+        return this.http.get(`${this.baseUrl}/locations/${id}`)
+            .map(res => {
+                return new Location().deserialize(res.json());
+            })
+            .catch(this.handleError);
+    }
+
+    updateLocation(location: Location) {
+        return this.http.put(`${this.baseUrl}/locations/${location.id}`, JSON.stringify(location), {headers: this.contentHeader})
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    createLocation(location: Location): Observable<Location> {
+        return this.http.post(`${this.baseUrl}/locations`, JSON.stringify(location), {headers: this.contentHeader})
+            .map(res => {
+                return new Location().deserialize(res.json());
+            })
+            .catch(this.handleError);
+    }
+
+    deleteLocation(id) {
+        return this.http.delete(`${this.baseUrl}/locations/${id}`, {headers: this.contentHeader})
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    handleError(error) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Locations server error');
+    }
+
 }
