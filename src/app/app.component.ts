24424fba21fc5923bd8fb98695c39fd8a6e0046a
Yash <yash@Yashs-MacBook-Pro.local>
2017-01-03 14:50:22 +0100

    SC-73: Onboarding slides Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,64 @@
-import { Component } from '@angular/core';
-import {Platform, Events} from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import {Component} from '@angular/core';
+import {Platform, Events, LoadingController} from 'ionic-angular';
+import {StatusBar, Splashscreen} from 'ionic-native';
+import {Storage} from '@ionic/storage';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import {TabsPage} from '../pages/tabs/tabs';
 import {AuthService} from "../services/auth.service";
 import {UserService} from "../services/user.service";
+import {IntroPage} from '../pages/intro/intro';
 
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+    template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = TabsPage;
+    rootPage: any = TabsPage;
+    loader: any;
 
-  constructor(platform: Platform, private authService: AuthService, private userService: UserService, private events: Events) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
+    constructor(platform: Platform, private authService: AuthService, private userService: UserService, private events: Events, public loadingCtrl: LoadingController, public storage: Storage) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+            Splashscreen.hide();
 
-      this.checkExistingToken();
-    });
-  }
+            this.checkExistingToken();
+
+            this.storage.get('introShown').then((result) => {
+
+                if (result) {
+                    this.rootPage = TabsPage;
+                } else {
+                    this.rootPage = IntroPage;
+                    this.storage.set('introShown', true);
+                }
+
+                this.loader.dismiss();
+            });
+        });
+
+        this.presentLoading();
+    }
 
 
-  checkExistingToken() {
-    this.events.subscribe('auth:refresh:user', () => this.refreshUser());
-    this.authService.checkExistingToken();
-  }
+    checkExistingToken() {
+        this.events.subscribe('auth:refresh:user', () => this.refreshUser());
+        this.authService.checkExistingToken();
+    }
 
-  refreshUser() {
-    this.userService.getUser().subscribe((user) => this.authService.setUser(user));
-  }
+    presentLoading() {
+
+        this.loader = this.loadingCtrl.create({
+            content: "Authenticating..."
+        });
+
+        this.loader.present();
+
+    }
+
+    refreshUser() {
+        this.userService.getUser().subscribe((user) => this.authService.setUser(user));
+    }
 
 }
