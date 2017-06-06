67f3a59f1f5ea5e03d73eb1b4cb45a0b26b570e9
lgallinaro <lgallina@thoughtworks.com>
2017-06-06 14:30:44 +0200

    [#146448113] Split configs for prod and test environments

---
+++ src/config.test-config.ts
@@ -0,0 +1,21 @@
+/**
+ * copy this file to config.ts and adjust values to your needs
+ */
+export const CONFIG = {
+    APP_VERSION: '1.0.3',
+
+    API_URL: 'https://api-test.shareandcharge.com/v1',
+    IMAGES_BASE_URL: 'https://api-test.shareandcharge.com',
+
+    API_KEY: {
+        param: 'apikey',
+        name: 'app',
+        secret: '{TEST_APP_SECRET}'
+    },
+    MIXPANEL_TOKEN: '{TEST_APP_MIXPANEL_TOKEN}',
+    FAQ_URL: 'https://www.shareandcharge.com/faq/',
+    CONTACT_EMAIL: 'info@shareandcharge.com',
+    TERMS_APP_URL: 'https://www.shareandcharge.com/app-agbs/',
+    TERMS_STATION_URL: 'https://www.shareandcharge.com/ladestation-agbs/',
+    DATA_PROTECTION_URL: 'https://www.shareandcharge.com/app-datenschutz/'
+};
