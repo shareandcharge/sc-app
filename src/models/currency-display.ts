984f6430feaaad42d741332f71f4a38de55d367c
Lauren Gallinaro and Sebastian Stontowski <lgallina@thoughtworks.com>
2017-07-20 09:08:14 +0200

    Write currency display pipe

---
+++ src/models/currency-display.ts
@@ -0,0 +1,26 @@
+import {Pipe} from '@angular/core';
+import {CurrencyService} from "../services/currency.service";
+
+@Pipe({
+    name: 'currencyDisplay'
+})
+export class CurrencyFormat {
+
+    currencyService: any =  CurrencyService;
+    globalCurrency: string = this.currencyService.getCurrency();
+
+    transform(value: number,
+        currencySign: string = this.globalCurrency,
+        decimalLength: number = 2,
+        chunkDelimiter: string = '.',
+        decimalDelimiter:string = ',',
+        chunkLength: number = 3): string {
+
+        value /= 100;
+
+        let result = '\\d(?=(\\d{' + chunkLength + '})+' + (decimalLength > 0 ? '\\D' : currencySign) + ')'
+        let num = value.toFixed(Math.max(0, ~~decimalLength));
+
+        return currencySign+(decimalDelimiter ? num.replace('.', decimalDelimiter) : num).replace(new RegExp(result, 'g'), '$&' + chunkDelimiter);
+    }
+}
