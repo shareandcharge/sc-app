4fa7dc8bf499309e2820a171c06c83fa32c05d98
Michael Fait <mfait@thoughtworks.com>
2017-08-02 14:22:51 +0200

    show msgs for api errors again

--- src/services/abstract.api.service.ts
+++ src/services/abstract.api.service.ts
@@ -1,48 +1,39 @@
 import {Injectable} from '@angular/core';
 import {Response} from "@angular/http";
 import {Observable} from "rxjs";
+
 import {TranslateService} from "@ngx-translate/core";
 import {ConfigService} from "./config.service";
 
 @Injectable()
 export abstract class AbstractApiService {
+
     protected baseUrl;
-    
+
     constructor(public configService: ConfigService, public translateService: TranslateService) {
         this.baseUrl = this.configService.getApiBaseUrl();
     }
 
     handleError(error: Response | any) {
-        let errMsg: string;
-        error = error || this.translateService.instant('error.unknown');
+        return Observable.throw(this.getErrorMessage(error));
+    }
 
+    private getErrorMessage(error: Response | any) {
         if (error instanceof Response) {
+
             if (error.status == 0) {
-                errMsg = this.translateService.instant('error_messages.no_connection');
-            }
-            else if (error.status >= 400) {
-                errMsg = '';
-            }
-            else {
-                let body;
+                return 'error_messages.no_connection';
+            } else {
                 try {
-                    body = error.json();
+                    let body = error.json();
+                    return 'api_error.' + body.key;
+                } catch (e) {
+                    return '';
                 }
-                catch (e) {
-                    body = error.text();
-                }
-
-                errMsg = body.key || body.message || JSON.stringify(body);
-                errMsg = "api_error." + errMsg;
-
-                // if (body.message) alert('DEBUG: ' + body.message);
             }
-        } else {
-            errMsg = error.message ? error.message : error.toString();
         }
 
-        // console.error('API error:', errMsg);
-        return Observable.throw(errMsg);
+        return '';
     }
 
 }
